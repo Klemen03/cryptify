36ea2a2fb6420d38b7ef62a646d46f1957c4820d
Klemen03 <klemenkrklec10@gmail.com>
2024-06-13 21:32:38 +0200

    Styled cryptocurrency list

--- app/components/CryptoList.jsx
+++ app/components/CryptoList.jsx
@@ -53,7 +53,7 @@ function CryptoList() {
   // console.log(searchedCoins);
 
   return (
-    <div className="flex flex-col h-screen pt-7 items-center dark:bg-gray-900">
+    <div className="flex flex-col min-h-screen pt-7 items-center dark:bg-gray-900">
       <h1 className="text-4xl text-amber-600   dark:text-yellow-300 font-medium tracking-wide">
         Cryptocurrencies by Market Cap
       </h1>
@@ -64,51 +64,50 @@ function CryptoList() {
         className="px-5 py-4 my-7 max-w-5xl w-full mx-auto outline-none border-2 dark:border-gray-500 dark:focus:border-amber-400 rounded-full dark:placeholder:focus:text-orange-200 focus:shadow-md dark:focus:shadow-orange-700 placeholder:tracking-wider transition-shadow-color duration-300 border-gray-200 focus:border-gray-800 focus:placeholder:text-gray-800 focus:shadow-gray-400"
       />
       <Table className="max-w-7xl mx-auto text-xl">
-        <TableCaption>A list of your recent invoices.</TableCaption>
+        {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
         <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Coin</TableHead>
-            <TableHead>Price</TableHead>
-            <TableHead>24h Change</TableHead>
-            <TableHead className="text-right">Market Cap</TableHead>
+          <TableRow className="flex flex-row items-center justify-between dark:hover:bg-gray-900 hover:bg-white">
+            <TableHead className="w-[100px] ml-16">Coin</TableHead>
+            <div className="flex flex-row items-center gap-x-14">
+              <TableHead>Price</TableHead>
+              <TableHead>24h Change</TableHead>
+              <TableHead className="text-right">Market Cap</TableHead>
+            </div>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {/* {searchedCoins.map((coin) => {
-            <TableRow key={coin.id}>
-              <TableCell>{coin.name}</TableCell>
-            </TableRow>;
-          })} */}
           {searchedCoins.map((coin) => (
             <TableRow key={coin.id}>
-              <Link id={coin.id} href={`/cryptocurrencies/${coin.id}`}>
-                <TableCell className="font-medium">
-                  <div className="flex">
-                    <img
-                      className="w-[50px] h-[50px]"
-                      src={coin.image}
-                      alt={coin.name}
-                    />
-                    <div className="flex flex-col pl-3 mr-14">
-                      <h1 className="uppercase tracking-wider">
-                        {coin.symbol}
-                      </h1>
-                      <p className="text-sm">{coin.name}</p>
-                    </div>
+              <Link
+                id={coin.id}
+                href={`/cryptocurrencies/${coin.id}`}
+                className="flex flex-row items-center justify-between p-3"
+              >
+                <TableCell className="font-medium flex">
+                  <img
+                    className="w-[50px] h-[50px]"
+                    src={coin.image}
+                    alt={coin.name}
+                  />
+                  <div className="flex flex-col pl-3 ">
+                    <h1 className="uppercase tracking-wider">{coin.symbol}</h1>
+                    <p className="text-sm">{coin.name}</p>
                   </div>
                 </TableCell>
                 <TableCell>
-                  {symbol}
-                  {coin.current_price.toLocaleString()}
-                </TableCell>
-                <TableCell>
-                  {coin.price_change_percentage_24h < 0 ? '-' : '+'}
-                  {Math.abs(coin.price_change_percentage_24h).toFixed(2)}
-                  {'%'}
-                </TableCell>
-                <TableCell className="text-right">
-                  {coin.market_cap.toLocaleString()}
-                  {'M'}
+                  <TableCell>
+                    {symbol}
+                    {coin.current_price.toLocaleString()}
+                  </TableCell>
+                  <TableCell>
+                    {coin.price_change_percentage_24h < 0 ? '-' : '+'}
+                    {Math.abs(coin.price_change_percentage_24h).toFixed(2)}
+                    {'%'}
+                  </TableCell>
+                  <TableCell>
+                    {coin.market_cap.toLocaleString()}
+                    {'M'}
+                  </TableCell>
                 </TableCell>
               </Link>
             </TableRow>
@@ -126,3 +125,91 @@ function CryptoList() {
 }
 
 export default CryptoList;
+
+// import {
+//   Table,
+//   TableBody,
+//   TableCaption,
+//   TableCell,
+//   TableFooter,
+//   TableHead,
+//   TableHeader,
+//   TableRow,
+// } from "@/components/ui/table"
+
+// const invoices = [
+//   {
+//     invoice: "INV001",
+//     paymentStatus: "Paid",
+//     totalAmount: "$250.00",
+//     paymentMethod: "Credit Card",
+//   },
+//   {
+//     invoice: "INV002",
+//     paymentStatus: "Pending",
+//     totalAmount: "$150.00",
+//     paymentMethod: "PayPal",
+//   },
+//   {
+//     invoice: "INV003",
+//     paymentStatus: "Unpaid",
+//     totalAmount: "$350.00",
+//     paymentMethod: "Bank Transfer",
+//   },
+//   {
+//     invoice: "INV004",
+//     paymentStatus: "Paid",
+//     totalAmount: "$450.00",
+//     paymentMethod: "Credit Card",
+//   },
+//   {
+//     invoice: "INV005",
+//     paymentStatus: "Paid",
+//     totalAmount: "$550.00",
+//     paymentMethod: "PayPal",
+//   },
+//   {
+//     invoice: "INV006",
+//     paymentStatus: "Pending",
+//     totalAmount: "$200.00",
+//     paymentMethod: "Bank Transfer",
+//   },
+//   {
+//     invoice: "INV007",
+//     paymentStatus: "Unpaid",
+//     totalAmount: "$300.00",
+//     paymentMethod: "Credit Card",
+//   },
+// ]
+
+// export function TableDemo() {
+//   return (
+//     <Table>
+//       <TableCaption>A list of your recent invoices.</TableCaption>
+//       <TableHeader>
+//         <TableRow>
+//           <TableHead className="w-[100px]">Invoice</TableHead>
+//           <TableHead>Status</TableHead>
+//           <TableHead>Method</TableHead>
+//           <TableHead className="text-right">Amount</TableHead>
+//         </TableRow>
+//       </TableHeader>
+//       <TableBody>
+//         {invoices.map((invoice) => (
+//           <TableRow key={invoice.invoice}>
+//             <TableCell className="font-medium">{invoice.invoice}</TableCell>
+//             <TableCell>{invoice.paymentStatus}</TableCell>
+//             <TableCell>{invoice.paymentMethod}</TableCell>
+//             <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+//           </TableRow>
+//         ))}
+//       </TableBody>
+//       <TableFooter>
+//         <TableRow>
+//           <TableCell colSpan={3}>Total</TableCell>
+//           <TableCell className="text-right">$2,500.00</TableCell>
+//         </TableRow>
+//       </TableFooter>
+//     </Table>
+//   )
+// }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header';
+
 import Banner from './components/Banner';
 import CryptoList from './components/CryptoList';
 
